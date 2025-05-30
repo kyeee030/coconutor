5adba8157289085ac89630fd54df85dd8396554b
112062109 <justinshih@gapp.nthu.edu.tw>
2025-05-30 08:35:16 +0200

    add time system

---
+++ assets/Script/Game/Environment/TimeSystem.ts
@@ -0,0 +1,64 @@
+const {ccclass, property} = cc._decorator;
+
+export enum TimeState {
+    DAY,
+    NIGHT,
+}
+
+@ccclass
+export default class TimeSystem extends cc.Component {
+    
+    @property
+    timeInterval: number = 5;
+
+    @property
+    dayTime: number = 100;
+
+    @property
+    nightTime: number = 100;
+
+    private timeState: TimeState = TimeState.DAY;
+    private gameTime: number = 0;
+
+    onLoad(){
+        this.gameTime = 0;
+        this.timeState = TimeState.DAY;
+    }
+
+    start(){
+        this.gameTime = 0;
+    }
+
+    private updateTimeStateMachine() {
+        if( this.timeState === TimeState.DAY && this.gameTime >= this.dayTime) {
+            this.gameTime = 0;
+            this.timeState = TimeState.NIGHT;
+            console.log("Transitioning to NIGHT");
+        }
+        else if(this.timeState === TimeState.NIGHT && this.gameTime >= this.nightTime) {
+            this.gameTime = 0;
+            this.timeState = TimeState.DAY;
+            console.log("Transitioning to DAY");
+        }
+    }
+
+    public getCurrentTimeState(): TimeState {
+        return this.timeState;
+    }
+
+    public changeCurrentTimeState(newState: TimeState) {
+        this.timeState = newState;
+        this.gameTime = 0;
+        console.log("Time state changed to: " + TimeState[this.timeState]);
+    }
+
+    public getGameTime(): number{
+        return this.gameTime;
+    }
+
+    public updateGameTime(dt: number){
+        this.gameTime += dt;
+        this.updateTimeStateMachine();
+    }
+
+}

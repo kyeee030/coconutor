0c00b8dc8b6c42e6568dbb58377aecd52f21521f
112062202 <st940601@gmail.com>
2025-06-02 10:54:12 +0800

    Add background and view indicator to minimap

--- assets/Script/Game/UI/Minimap.ts
+++ assets/Script/Game/UI/Minimap.ts
@@ -8,14 +8,18 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class MinimapCamera extends cc.Component {
+export default class Minimap extends cc.Component {
 
     // LIFE-CYCLE CALLBACKS:
-    @property(cc.Node)
-    minimap: cc.Node = null;
+
+    @property(Number)
+    minimapSize: number = 1.0;
 
     @property(cc.Node)
     mapRenderObject: cc.Node = null;
+
+    @property(cc.Node)
+    minimapCamera: cc.Node = null;
 
     private _camera: cc.Camera = null;
     private _renderTexture: cc.RenderTexture = null;
@@ -23,10 +27,10 @@ export default class MinimapCamera extends cc.Component {
     private _renderSprite: cc.Sprite = null;
 
     onLoad () {
-        this._camera = this.getComponent(cc.Camera);
+        this._camera = this.minimapCamera.getComponent(cc.Camera);
         this._renderSprite = this.mapRenderObject.getComponent(cc.Sprite);
         if (!this._camera || !this._renderSprite) {
-            this.minimap.active = false;
+            this.node.active = false;
             cc.error("MinimapCamera: Camera component not found!");
             return;
         }
@@ -36,12 +40,16 @@ export default class MinimapCamera extends cc.Component {
         this._renderTexture.initWithSize(512, 512, cc.Texture2D.PixelFormat.RGBA8888);
         this._camera.targetTexture = this._renderTexture;
 
+
         this._renderSpriteFrame = new cc.SpriteFrame();
         this._renderSpriteFrame.setTexture(this._renderTexture);
         this.mapRenderObject.getComponent(cc.Sprite).spriteFrame = this._renderSpriteFrame;
         
-        this.minimap.active = true;
+        this.node.active = true;
         this._renderSprite.spriteFrame = this._renderSpriteFrame;
+
+        this.node.scaleX = this.minimapSize;
+        this.node.scaleY = this.minimapSize;
     }
 
     start () {}

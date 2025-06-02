c63afad4011f723d401e16ebc84ee5a3ae3ea8ce
112062335 <backup31415927@gmail.com>
2025-06-02 14:23:38 +0800

    bfs and move complete(no consider no path)

--- assets/Script/Game/Enemy/Enemy.ts
+++ assets/Script/Game/Enemy/Enemy.ts
@@ -21,6 +21,12 @@ export default class Enemy extends cc.Component {
     enemyState: EnemyState;
     //when testing property you can change it at the right side :)
 
+    //---test----//
+    @property
+    testProperty: cc.Vec2 = cc.v2(0, 0); //if you need :)
+
+    //---test----//
+
     @property       
     hp: number = 0; //if you need :)    
     @property
@@ -33,22 +39,22 @@ export default class Enemy extends cc.Component {
     @property
     attackRange: number = 0;
 
-    _pathPlanning: cc.Node;
+    _pathPlanning: any;
+    _nextPos: cc.Vec2 = cc.v2(0, 0);
 
     target: {
         dist: number,
-        pos: {
-            x: number,
-            y: number
-        }
+        pos: cc.Vec2,
         type: BuildingState,
         tag: null //Index of building or somwthing else
     }
 
     onLoad () {
-        this._pathPlanning = cc.find('PathPlanning');
+        this._pathPlanning = cc.find('Canvas/PathPlanning');
+        
+        this._pathPlanning = this._pathPlanning ? this._pathPlanning.getComponent('PathPlanning') : null;
         if (!this._pathPlanning) {
-            console.error('Can\'t find PathPlanning node, please check the scene!');
+            console.error('Can\'t find PathPlanning script, please check the scene!');
         }
     }
 
@@ -74,19 +80,19 @@ export default class Enemy extends cc.Component {
     }
 
     update (dt) {
-        this.findTarget();
+        const selfpos = this.findTarget();
         if (this.target) {
             if (this.hp < 0) {
 
             } else if(this.target.dist < this.attackRange) {
                 this.enemyState = EnemyState.ATTACK
             } else {
-                this.enemyState = EnemyState.ATTACK
+                this.enemyState = EnemyState.MOVE;
             }
             switch (this.enemyState) {
                 case EnemyState.MOVE:
                 case EnemyState.ATTACK:
-                    this.findDirection(); 
+                    this.findDirection(selfpos); 
                 break;
                 case EnemyState.DIE:
                     this.death();
@@ -99,21 +105,56 @@ export default class Enemy extends cc.Component {
     }
 
 
-    findTarget () { //find the target
-
+    findTarget () { //find the target and return self pos
+        if (!this._pathPlanning) 
+            cc.error('Can\'t find PathPlanning script');
+        let pos_t = this._pathPlanning.findLocation(this.node.position.x, this.node.position.y);
+        if (!pos_t) 
+            cc.error('Enemy can\'t find location, please check it position is valid!');
+        const buildings = [this.testProperty];
+        const x = pos_t.x;
+        const y = pos_t.y;
+        let minDist = Infinity;
+        for (let b of buildings) {
+            const dist = Math.sqrt(Math.pow(b.x - x, 2) + Math.pow(b.y - y, 2));
+            if(dist < minDist) {
+                minDist = dist;
+                this.target = {
+                    dist: dist,
+                    pos: new cc.Vec2(b.x, b.y),
+                    type: null, //or other state
+                    tag: null //Index of building or something else
+                };
+            }
+        }
+        return pos_t;
     }
 
-    findDirection () { //find the direction
-        /*if (target.dist < attackRange)
-            this.schedule(this.attack, this.coolDown);
+    findDirection (selfpos: cc.Vec2) { //find the direction
+        if (this.target.dist < this.attackRange)
+        {
+            //this.schedule(this.attack, this.coolDown);
+            return;
+        }
         else
             this.unschedule(this.attack);
-            this.move();
-        */
+        if((Math.pow(Math.abs(this.node.position.x - this._nextPos.x), 2) + Math.pow(Math.abs(this.node.position.y - this._nextPos.y), 2) < 10))
+        {
+            let pos_t = this._pathPlanning.findLocation(this.node.position.x, this.node.position.y);
+            this._nextPos = this._pathPlanning.findPath(selfpos, this.target.pos);
+        }
+        this.move(this._nextPos);
     }
 
-    move () { //move
-
+    move (nextPos: cc.Vec2) { //move
+        if (!nextPos) {
+            cc.error('Can\'t find direction, please check the path planning!');
+            return;
+        }   
+        let angle = Math.atan2(nextPos.y - this.node.position.y, nextPos.x - this.node.position.x);
+        //rotation();
+        this.node.x += Math.cos(angle) * this.speed;
+        this.node.y += Math.sin(angle) * this.speed;
     }
 
     attack () { //attack & call animation
--- assets/Script/Game/Enemy/Path/PathPlanning.ts
+++ assets/Script/Game/Enemy/Path/PathPlanning.ts
@@ -57,13 +57,67 @@ export default class PathPlanning extends cc.Component {
         cc.log(this._nodes);
     }
 
-    findBlock(x: number, y: number): cc.Vec2 | null {
-        const xi = Math.floor(x);
-        const yi = Math.floor(y);
-        if (xi < 0 || xi >= this.mapSize.x || yi < 0 || yi >= this.mapSize.y) {
+    findLocation(x: number, y: number): cc.Vec2 | null {
+        const xi = Math.floor((x - this.startPoint.x) / this.pixelSize.x + 0.5);
+        const yi = Math.floor((y - this.startPoint.y) / this.pixelSize.y + 0.5);
+        if (xi < this.startPoint.x || xi >= this.mapSize.x || yi < this.startPoint.y || yi >= this.mapSize.y) {
             return null;
         }
         return new cc.Vec2(xi, yi);
     }
 
+    findPath(start: cc.Vec2, end: cc.Vec2): cc.Vec2 {  //return the direction
+        if(start.equals(end)) return new cc.Vec2(this.startPoint.x + start.x * this.pixelSize.x, this.startPoint.y + start.y * this.pixelSize.y);   
+        let dir = [
+            new cc.Vec2(1, 0),
+            new cc.Vec2(-1, 0),
+            new cc.Vec2(0, 1),
+            new cc.Vec2(0, -1),
+            new cc.Vec2(1, 1),
+            new cc.Vec2(1, -1),
+            new cc.Vec2(-1, 1),
+            new cc.Vec2(-1, -1)
+        ];
+        let queue: cc.Vec3[] = [];
+        let visited: boolean[][] = Array.from({ length: this.mapSize.x }, () => Array(this.mapSize.y).fill(false));
+        for(let it of dir) {
+            let newX = start.x + it.x;
+            let newY = start.y + it.y;
+            if (this.checkValid(newX, newY) && !visited[newX][newY]) {
+                const idx = dir.indexOf(it);
+                queue.push(new cc.Vec3(newX, newY, idx));
+                visited[newX][newY] = true;
+            }
+        }
+        while (queue.length > 0) {
+            let current = queue.shift();
+            if (!current) continue;
+            if (current.x === end.x && current.y === end.y) {
+                return new cc.Vec2(
+                    this.startPoint.x + (dir[current.z].x + start.x) * this.pixelSize.x,
+                    this.startPoint.y + (dir[current.z].y + start.y) * this.pixelSize.y
+                );
+            }
+            for (let it of dir) {
+                let newX = current.x + it.x;
+                let newY = current.y + it.y;
+                if (this.checkValid(newX, newY) && !visited[newX][newY]) {
+                    queue.push(new cc.Vec3(newX, newY, current.z));
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+        return new cc.Vec2(0, 0);
+    }
+
+    checkValid(x: number, y: number): boolean {
+        if (x < 0 || x >= this.mapSize.x || y < 0 || y >= this.mapSize.y) {
+            return false;
+        }
+        return !(this._nodes[x][y].isBlocked);
+    }
+
+    calculateDistance(selfPos: cc.Vec2, targetPos: cc.Vec2): number {
+        return Math.sqrt(Math.pow(selfPos.x - (this.startPoint.x + (targetPos.x) * this.pixelSize.x), 2) + Math.pow(selfPos.y - (this.startPoint.y + (targetPos.y) * this.pixelSize.y), 2));
+    }
 }

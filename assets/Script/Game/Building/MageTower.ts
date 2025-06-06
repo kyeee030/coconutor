9eea796ede83a6451137b08bcd4f84e47b309e4a
112062202 <st940601@gmail.com>
2025-06-07 07:07:30 +0800

    finish mage tower

--- assets/Script/Game/Building/MageTower.ts
+++ assets/Script/Game/Building/MageTower.ts
@@ -2,6 +2,7 @@ import Building, { BuildingState } from "./Building";
 import Targeting from "../Misc/Targeting";
 const { ccclass, property } = cc._decorator;
 import SwordBullet from "../Misc/SwordBullet";
+import MagicBullet from "../Misc/MagicBullet";
 
 @ccclass
 export default class MageTower extends Building {
@@ -34,58 +35,54 @@ export default class MageTower extends Building {
         // this._animation = this.headNode.getComponent(cc.Animation);
         
         
-        // if(!this._canvas)
-        //     this._canvas = cc.find("Canvas");
+        if(!this._canvas)
+            this._canvas = cc.find("Canvas");
 
-        // if(!this._targetingSystem && this.rangeNode) {
-        //     this._targetingSystem = this.rangeNode.getComponent(Targeting);
-        //     if (!this._targetingSystem) {
-        //         console.error("Targeting component not found on rangeNode.");
-        //     } else {
-        //         this._targetingSystem.selfNode = this.node; // 設置 selfNode 為當前建築物節點
-        //     }
-        // }
+        if(!this._targetingSystem && this.rangeNode) {
+            this._targetingSystem = this.rangeNode.getComponent(Targeting);
+            if (!this._targetingSystem) {
+                console.error("Targeting component not found on rangeNode.");
+            } else {
+                this._targetingSystem.selfNode = this.node; // 設置 selfNode 為當前建築物節點
+            }
+        }
 
         // this.attack();
     }
 
     update(dt) {
-        // // super.update(dt);
-        // // console.log("target: ", this._targetingSystem.getTargets());
-        // if(!cc.isValid(this._targetNode)) {
-        //     this._targetNode = null;
-        // }
-        // const targets = this._targetingSystem.getTargets();
-        // if(targets.length > 0) {
-        //     while(!targets[0] || !cc.isValid(targets[0])){
-        //         targets.shift(); // 移除空的目標
-        //     }
-        //     if(targets[0] != this._targetNode) {
-        //         this._targetNode = targets[0];
-        //         this.schedule(()=>{
-        //             this.attack();
-        //         }, this.attackSpeed);
-        //     }
-        // } else {
-        //     this._targetNode = null;
-        //     this.unschedule(this.attack);
-        //     this.unscheduleAllCallbacks();
-        //     cc.log("gg");
-        // }
+        if(!cc.isValid(this._targetNode)) {
+            this._targetNode = null;
+        }
+        const targets = this._targetingSystem.getTargets();
+        if(targets.length > 0) {
+            while(!targets[0] || !cc.isValid(targets[0])){
+                targets.shift(); // 移除空的目標
+            }
+            if(targets[0] != this._targetNode) {
+                this._targetNode = targets[0];
+                this.schedule(()=>{
+                    this.attack();
+                }, this.attackSpeed);
+            }
+        } else {
+            this._targetNode = null;
+            this.unschedule(this.attack);
+            this.unscheduleAllCallbacks();
+        }
 
-        // if(this._targetNode != null && cc.isValid(this._targetNode) && this.headNode) {
-        //     const targetDirection = cc.v2(this._targetNode.position.x - this.node.position.x,
-        //     this._targetNode.position.y - this.node.position.y).normalize();
-        //     const angle = cc.misc.radiansToDegrees(Math.atan2(targetDirection.y, targetDirection.x));
-        //     this.headNode.angle = angle - 90;
-        // } else {
-        //     this.headNode.angle += 1;
-        // }
+        if(this._targetNode != null && cc.isValid(this._targetNode) && this.headNode) {
+            const targetDirection = cc.v2(this._targetNode.position.x - this.node.position.x,
+            this._targetNode.position.y - this.node.position.y).normalize();
+            const angle = cc.misc.radiansToDegrees(Math.atan2(targetDirection.y, targetDirection.x));
+            this.headNode.angle = angle - 90;
+        } else {
+            this.headNode.angle += 1;
+        }
     }
 
     attack(): void {
         // console.log("Turret is attacking!");
-        this._animation.play("TurretAttack");
         super.attack(); // 呼叫父類別的攻擊方法
     }
 
@@ -93,7 +90,7 @@ export default class MageTower extends Building {
         // console.log(`Turret creates a bullet with damage:`);
         const bulletNode = cc.instantiate(this.bullet);
         bulletNode.setPosition(this.node.position); 
-        const bulletComponent = bulletNode.getComponent(SwordBullet); 
+        const bulletComponent = bulletNode.getComponent(MagicBullet); 
         bulletComponent.damage = this.damage;
         bulletComponent.setTarget(this._targetNode);
         this._canvas.addChild(bulletNode); 
